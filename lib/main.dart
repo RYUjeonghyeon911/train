6058ff7b6d450a7752ed02d4e866192cda53866f
RYUjeonghyeon911 <jeonghyeonairking@gmail.com>
2024-11-13 20:00:59 +0900

    Fixed main.dart and widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,384 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(TrainApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TrainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Train Booking App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  String? departureStation;
+  String? arrivalStation;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('기차 예매'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('출발역',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      GestureDetector(
+                        onTap: () async {
+                          String? result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StationListPage(),
+                            ),
+                          );
+                          if (result != null) {
+                            setState(() {
+                              departureStation = result;
+                            });
+                          }
+                        },
+                        child: Text(
+                          departureStation ?? '선택',
+                          style: TextStyle(
+                            fontSize: 40,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 2,
+                    height: 50,
+                    color: Colors.grey[400],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('도착역',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      GestureDetector(
+                        onTap: () async {
+                          String? result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StationListPage(),
+                            ),
+                          );
+                          if (result != null) {
+                            setState(() {
+                              arrivalStation = result;
+                            });
+                          }
+                        },
+                        child: Text(
+                          arrivalStation ?? '선택',
+                          style: TextStyle(
+                            fontSize: 40,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: departureStation != null && arrivalStation != null
+                  ? () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SeatPage(
+                            departureStation: departureStation ?? '',
+                            arrivalStation: arrivalStation ?? '',
+                          ),
+                        ),
+                      );
+                    }
+                  : null,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                child: Text(
+                  '좌석 선택',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.grey[200],
+    );
+  }
+}
+
+class StationListPage extends StatelessWidget {
+  final List<String> stations = [
+    "수서",
+    "동탄",
+    "평택지제",
+    "천안아산",
+    "오송",
+    "대전",
+    "김천구미",
+    "동대구",
+    "경주",
+    "울산",
+    "부산"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('출발역'),
+      ),
+      body: ListView.builder(
+        itemCount: stations.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              stations[index],
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context, stations[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SeatPage extends StatefulWidget {
+  final String departureStation;
+  final String arrivalStation;
+
+  SeatPage({required this.departureStation, required this.arrivalStation});
+
+  @override
+  _SeatPageState createState() => _SeatPageState();
+}
+
+class _SeatPageState extends State<SeatPage> {
+  List<List<bool>> seats = List.generate(10, (index) => List.generate(4, (index) => false));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('좌석 선택'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.departureStation,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(Icons.arrow_forward, size: 30, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text(
+                    widget.arrivalStation,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 24,
+                    height: 24,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(width: 4),
+                  Text('선택됨'),
+                  SizedBox(width: 20),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    color: Colors.grey[300],
+                  ),
+                  SizedBox(width: 4),
+                  Text('선택 안됨'),
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                children: List.generate(10, (rowIndex) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ...List.generate(2, (colIndex) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              seats[rowIndex][colIndex] = !seats[rowIndex][colIndex];
+                            });
+                          },
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: seats[rowIndex][colIndex] ? Colors.purple : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        );
+                      }),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          '${rowIndex + 1}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      ...List.generate(2, (colIndex) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              seats[rowIndex][colIndex + 2] = !seats[rowIndex][colIndex + 2];
+                            });
+                          },
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: seats[rowIndex][colIndex + 2] ? Colors.purple : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        );
+                      }),
+                    ],
+                  );
+                }),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: seats.any((row) => row.contains(true))
+                    ? () {
+                        String selectedSeat = '';
+                        for (int i = 0; i < seats.length; i++) {
+                          for (int j = 0; j < seats[i].length; j++) {
+                            if (seats[i][j]) {
+                              selectedSeat = '${i + 1}-${String.fromCharCode(65 + j)}';
+                              break;
+                            }
+                          }
+                          if (selectedSeat.isNotEmpty) break;
+                        }
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('예매 확인'),
+                            content: Text('예매 하시겠습니까?\n좌석: $selectedSeat'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('취소'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).popUntil((route) => route.isFirst);
+                                },
+                                child: Text('확인'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    : null,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  child: Text(
+                    '예매 하기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
